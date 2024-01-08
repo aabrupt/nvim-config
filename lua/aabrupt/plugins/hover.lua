7634d97b536911838e4d80dd68b16f20b19ab77a
Ludvig Källqvist Nygren <lukecastellan165@gmail.com>
2024-01-08 16:46:21 +0100

    Revert: Mouse hovering causing error

--- lua/aabrupt/plugins/hover.lua
+++ lua/aabrupt/plugins/hover.lua
@@ -19,8 +19,5 @@ return {
 
         vim.keymap.set("n", "K", hover.hover, {desc="Hover Symbol"})
         vim.keymap.set("n", "gK", hover.hover_select, {desc="Select Information Provider And Hover"})
-
-        vim.keymap.set('n', '<MouseMove>', require('hover').hover_mouse, { desc = "hover.nvim (mouse)" })
-        vim.o.mousemoveevent = true
     end
 }
